40e3319800c5db6fe6aa9a6ecb1a935ee7353fee
Salleeem <mateusonego@gmail.com>
2023-08-29 14:06:58 -0300

    dksao

--- js/Carrinho.js
+++ js/Carrinho.js
@@ -1,145 +1,46 @@
-let openShopping = document.querySelector('.shopping');
-let closeShopping = document.querySelector('.closeShopping');
-let list = document.querySelector('.list');
-let listCard = document.querySelector('.listCard');
-let body = document.querySelector('body');
-let total = document.querySelector('.total');
-let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', ()=>{
-    body.classList.add('active');
-})
-closeShopping.addEventListener('click', ()=>{
-    body.classList.remove('active');
-})
+    
+        const produtos = [
+            { nome: "Cotton T-shirt", preco: 44.00, imagem: "https://i.imgur.com/1GrakTl.jpg" },
+            { nome: "Cotton T-shirt", preco: 44.00, imagem: "https://i.imgur.com/ba3tvGm.jpg" },
+            { nome: "Cotton T-shirt", preco: 44.00, imagem: "https://i.imgur.com/pHQ3xT3.jpg" }
+        ];
 
-let products = [
-    {
-        id: 1,
-        name: 'Garrafa Térmica Switchback | 473mL',
-        image: '1.PNG',
-        price: 235
-    },
-    {
-        id: 2,
-        name: 'Garrafa Térmica Switchback | 473mL',
-        image: '2.PNG',
-        price: 235
-    },
-    {
-        id: 3,
-        name: 'Garrafa Térmica Classic Hydration | 750mL',
-        image: '3.PNG',
-        price: 270
-    },
-    {
-        id: 4,
-        name: 'Mug Térmico Classic Stanley | 354mL',
-        image: '4.PNG',
-        price: 199
-    },
-    {
-        id: 5,
-        name: 'Garrafa Térmica Quick Flip Stanley | 710mL',
-        image: '5.PNG',
-        price: 245
-    },
-    {
-        id: 6,
-        name: 'Garrafa Térmica Quick Flip Stanley | 710mL',
-        image: '6.PNG',
-        price: 245
-    },
-    {
-        id: 7,
-        name: 'Garrafa Térmica Flip Straw Stanley Lagoon | 651mL',
-        image: '7.PNG',
-        price: 269
-    },
-    {
-        id: 8,
-        name: 'Tumbler Térmico Flip Straw Stanley Lagoon | 887mL',
-        image: '8.PNG',
-        price: 295
-    },
-    {
-        id: 9,
-        name: 'Jug Térmico Flip Straw Stanley Lagoon | 1,2L',
-        image: '9.PNG',
-        price: 375
-    },
-    {
-        id: 10,
-        name: 'Copo Quencher 2.0 Rose Quartz | 1,18L',
-        image: '10.PNG',
-        price: 315
-    },
-    {
-        id: 11,
-        name: 'Garrafa Térmica Flip Straw Kids Stanley | 500mL',
-        image: '11.PNG',
-        price: 245
-    },
-    {
-        id: 12,
-        name: 'Garrafa Térmica Quick Flip Stanley | 710mL',
-        image: '12.PNG',
-        price: 245
-    }
-];
-let listCards  = [];
-function initApp(){
-    products.forEach((value, key) =>{
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('item');
-        newDiv.innerHTML = `
-            <img src="img/produtos/${value.image}">
-            <div class="title">${value.name}</div>
-            <div class="price">R$ ${value.price.toLocaleString()},00</div>
-            <button onclick="addToCard(${key})" class="card-button">Adicionar ao carrinho</button>`;
-        list.appendChild(newDiv);
-    })
-}
-initApp();
-function addToCard(key){
-    if(listCards[key] == null){
-        // copy product form list to list card
-        listCards[key] = JSON.parse(JSON.stringify(products[key]));
-        listCards[key].quantity = 1;
-    }
-    reloadCard();
-}
-function reloadCard(){
-    listCard.innerHTML = '';
-    let count = 0;
-    let totalPrice = 0;
-    listCards.forEach((value, key)=>{
-        totalPrice = totalPrice + value.price;
-        count = count + value.quantity;
-        if(value != null){
-            let newDiv = document.createElement('li');
-            newDiv.innerHTML = `
-                <div><img src="img/produtos/${value.image}"/></div>
-                <div>${value.name}</div>
-                <div><strong>R$${value.price.toLocaleString()},00</strong></div>
-                <div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-                    <div class="count">${value.quantity}</div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-                </div>
-                </br>`;
-                listCard.appendChild(newDiv);
+        let carrinho = [];
+
+        function atualizarCarrinho() {
+            const carrinhoContainer = document.querySelector(".cart");
+
+            carrinhoContainer.innerHTML = ""; // Limpa o conteúdo anterior do carrinho
+
+            for (let i = 0; i < carrinho.length; i++) {
+                const produto = carrinho[i];
+
+                const produtoHTML = `
+                    <div class="row main align-items-center">
+                        <div class="col-2"><img class="img-fluid" src="${produto.imagem}"></div>
+                        <div class="col">
+                            <div class="row text-muted">${produto.nome}</div>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="remove-from-cart border">Remover</a>
+                        </div>
+                        <div class="col">&euro; ${produto.preco.toFixed(2)}</div>
+                    </div>
+                `;
+
+                carrinhoContainer.insertAdjacentHTML("beforeend", produtoHTML);
+
+                // Adiciona evento de remoção ao botão "Remover" deste produto
+                const botoesRemover = carrinhoContainer.querySelectorAll(".close");
+                const ultimoBotaoRemover = botoesRemover[botoesRemover.length - 1];
+
+                ultimoBotaoRemover.addEventListener("click", () => removerProduto(i));
+            }
+
+            // Calcule e atualize totais, quantidades, etc. e exiba onde necessário
         }
-    })
-    total.innerText = "R$"+totalPrice.toLocaleString()+",00";
-    quantity.innerText = count;
-}
-function changeQuantity(key, quantity){
-    if(quantity == 0){
-        delete listCards[key];
-    }else{
-        listCards[key].quantity = quantity;
-        listCards[key].price = quantity * products[key].price;
-    }
-    reloadCard();
-}
+
+        // Restante do código (adicionarProduto, removerProduto) permanece igual
+   
+
